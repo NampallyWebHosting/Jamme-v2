221f640914e4e67c45796af859f9d1048db2bec3
Deveshwar-Jamme <deveshwar@jamme.app>
2025-03-03 16:08:38 +0530

    added mixpannel tracking

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+
 import ConfirmationPage from '@/pages/ConfirmationPage';
 import BlogApp from '@/pages/Csae';
 import EulaPage from '@/pages/Eula';
@@ -9,11 +12,34 @@ import PrivacyPage from '@/pages/privacy';
 import SurveyForm from '@/pages/SurveyForm';
 import TermConditionPage from '@/pages/TermCondition';
 import WaitListPage from '@/pages/WaitListPage';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Mixpanel from "@/components/internal/mixpanel/mixpanel";
+
+const RouteTracker = () => {
+  const location = useLocation();
+  const [startTime, setStartTime] = useState<number>(Date.now());
+
+  useEffect(() => {
+    // When the user navigates to a new page
+    const page = location.pathname;
+    Mixpanel.track("Page View", { page });
+
+    // Reset start time
+    setStartTime(Date.now());
+
+    // Track time spent when leaving the page
+    return () => {
+      const timeSpent = (Date.now() - startTime) / 1000; // Convert to seconds
+      Mixpanel.track("Time Spent on Page", { page, timeSpent });
+    };
+  }, [location]);
+
+  return null;
+};
 
 const RoutesIndex = () => {
   return (
     <Router>
+      <RouteTracker /> {/* Tracks page views & time spent */}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/surveyform" element={<SurveyForm />} />
